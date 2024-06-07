c2fee52345c460d5240dc26b71c5149f9f049e60
khushboo jain <khushiejain92@gmail.com>
2024-06-07 18:31:32 +0530

    creation of blog app and establishing connection

---
+++ crashblog/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length= 255)
+    #field for storing the title of the post
+    slug = models.SlugField() 
+    # it is the internet address for title
+    intro = models.TextField()
+    #for storing longer texts than charfield
+    body= models.TextField()
+    #contains detail page of the blog
+    created_at = models.DateTimeField(auto_now_add=True)
+    #tracks time when the post was published
--- crashblog/core/views.py
+++ crashblog/core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from blog.models import Post
+#since we are not in the same folder as the models file
+
 # Create your views here.
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    posts=Post.objects.all()
+    
+    return render(request, 'core/frontpage.html',{'posts': posts}) 
+    #creates a dictionary here
 
 def aboutpage(request):
     return render(request, 'core/aboutpage.html')
+
