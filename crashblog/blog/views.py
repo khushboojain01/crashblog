b52ba98c31e77239d442b42cb59bc713d976725e
khushboo jain <khushiejain92@gmail.com>
2024-06-09 17:28:21 +0530

    connection established : comments can be added on each post

--- crashblog/blog/views.py
+++ crashblog/blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from .forms import CommentForm
+from django.shortcuts import redirect
+
 
 def detail(request, slug):  # address for the blog post
     post = get_object_or_404(Post, slug=slug)  # to get post based on slug
     # if doesn't get post from slug, throws error 404
 
-    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit = False) #will create a temporary comment for us
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
 
     return render(request, 'blog/detail.html', {'post': post, 'form': form })
