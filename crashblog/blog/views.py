1ec37317b3449d5bef073047f4824c643b996cc9
khushboo jain <khushiejain92@gmail.com>
2024-06-09 16:54:25 +0530

    creation of comment form

--- crashblog/blog/views.py
+++ crashblog/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from .forms import CommentForm
 
 def detail(request, slug):  # address for the blog post
     post = get_object_or_404(Post, slug=slug)  # to get post based on slug
     # if doesn't get post from slug, throws error 404
 
-    return render(request, 'blog/detail.html', {'post': post})
+    form = CommentForm()
+
+    return render(request, 'blog/detail.html', {'post': post, 'form': form })
