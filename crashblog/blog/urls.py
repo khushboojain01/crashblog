688b9c5d66fefad125f644e4c7f2a11cb95cdc03
khushboo jain <khushiejain92@gmail.com>
2024-06-11 20:52:07 +0530

    successfully interlinked the posts with categories

--- crashblog/blog/urls.py
+++ crashblog/blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<slug:slug>/', views.detail, name='post_detail'),
+    path('<slug:category_slug>/<slug:slug>', views.detail, name='post_detail'),
+    path('<slug:slug>/', views.category, name='category_detail'),
 ]
 #the <slug:slug> second slug is from views.py (request, slug)
 
--- crashblog/blog/views.py
+++ crashblog/blog/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Post
+from .models import Post, Category
 from .forms import CommentForm
 from django.shortcuts import redirect
 
-
-def detail(request, slug):  # address for the blog post
+def detail(request, category_slug, slug):  # address for the blog post
     post = get_object_or_404(Post, slug=slug)  # to get post based on slug
     # if doesn't get post from slug, throws error 404
 
@@ -12,12 +11,20 @@ def detail(request, slug):  # address for the blog post
         form = CommentForm(request.POST)
 
         if form.is_valid():
-            comment = form.save(commit = False) #will create a temporary comment for us
+            comment = form.save(commit=False)  # will create a temporary comment for us
             comment.post = post
             comment.save()
 
-            return redirect('post_detail', slug=slug)
+            return redirect('post_detail', category_slug=category_slug, slug=slug)
     else:
         form = CommentForm()
 
-    return render(request, 'blog/detail.html', {'post': post, 'form': form })
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
+
+def category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    
+    return render(request, 'blog/category.html', {'category': category})
+
+
+
