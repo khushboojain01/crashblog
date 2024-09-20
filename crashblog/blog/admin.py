7c30bd86086c9866ef544d45a150b6c4f137ef0a
Khushboo Jain <khushboo.j@Khushboos-MacBook-Air.local>
2024-09-20 23:02:10 +0530

    added searchfield, filtering, and auto completion of slug in django admin

--- crashblog/blog/admin.py
+++ crashblog/blog/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 
-from .models import Post, Category
+from .models import Post, Category , Comment
+
+class CommentItemInLine(admin.TabularInline):
+    model = Comment
+    raw_id_fields = ['post']
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'intro', 'body']
     list_display = ['title', 'slug', 'category','created_at']
+    list_filter = ['category','created_at']
+    inlines = [ CommentItemInLine ]
+    prepopulated_fields = {'slug': ('title', )}
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_filter = ['title']
+    prepopulated_fields = {'slug': ('title', )}
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'post', 'created_at']
+    
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Category)
-
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment,CommentAdmin)
 # Register your models here.
