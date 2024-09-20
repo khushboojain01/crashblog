15160d3b5f9d70cf82517931797504c127baee9e
Khushboo Jain <khushboo.j@Khushboos-MacBook-Air.local>
2024-09-20 22:09:54 +0530

    adding searchfields and category in admin

--- crashblog/blog/admin.py
+++ crashblog/blog/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from .models import Post, Category
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'body']
+    list_display = ['title', 'slug', 'category','created_at']
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 
 # Register your models here.
